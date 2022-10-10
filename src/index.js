6ad9200366ade77cd4a3e4582541aafcd7f7dbe8
Dratatin <matchirouze@orange.fr>
2022-10-11 01:55:01 +0200

    ellipsis for dynamic dropdown width

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ root.render(
   <React.StrictMode>
     <div className="demo">
       <Dropdown
-        options={["option1", "option2", "option3", "option4", "option5"]}
+        options={["option1", "option2", "option3", "option4option4option4", "option5"]}
       />
     </div>
   </React.StrictMode>
